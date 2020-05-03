3ff759a32b2c894b86b78df2809fce40e327b94f
OnkarSD <omkardherange1@gmail.com>
2020-05-03 07:22:02 +0530

    Created initial components

---
+++ src/components/EachShelf.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EachShelf() {
+  return <div />;
+}
+
+export default EachShelf;
---
+++ src/components/SearchBook.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import PropTypes from "prop-types";
+
+class SearchBook extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default SearchBook;
